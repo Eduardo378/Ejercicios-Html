ea823647d46ab35bbe6e168004b2d15c6d7259c2
Wilson <wilsongonzaleza140gmail.com>
2022-09-02 16:17:57 -0400

    Agregando los archivos de los ejercicios realizados y practicas

---
+++ js/cuestionario.js
@@ -0,0 +1,4 @@
+var nombre = prompt("¿Cuál es su nombre?");
+var apellido = prompt("¿Cuál es tu apellido?");
+document.write("<p> Your Name is: " + "<i>" + nombre + "</i>" + "</p>");
+document.write("<p> Your Lastname is: " + "<i>" + apellido + "</i>" + "</p>");
